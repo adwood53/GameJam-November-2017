9d1f2eca94f313299d8a9064606f61b1cc4ca19c
adwood53 <a.j.woodward53@gmail.com>
2017-11-12 19:19:45 +0000

    Added Menus and Levels
    Menu and Level 1 & 2

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour {
+
+	public Button playButton;
+
+	// Use this for initialization
+
+	void Start() {
+
+		Button btn = playButton.GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnClick);
+
+	}
+
+	void TaskOnClick () {
+
+		SceneManager.LoadScene("Level1");
+		Debug.Log ("WHY");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
